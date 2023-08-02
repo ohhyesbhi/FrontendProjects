1a8d825e6a4c656bcdadcbd16aee899252a96132
unknown <kamatabhishek14@gmail.com>
2023-08-03 00:59:56 +0530

    StreamUp

---
+++ StreamUp-main/StreamUp-main/src/utils/fetchFromAPI.js
@@ -0,0 +1,19 @@
+import axios from 'axios';
+
+export const BASE_URL = 'https://youtube-v31.p.rapidapi.com';
+
+const options = {
+  params: {
+    maxResults: 50,
+  },
+  headers: {
+    'X-RapidAPI-Key': "41bbbf7cfemsh416187e41242213p193256jsnb856b6b71278",
+    'X-RapidAPI-Host': 'youtube-v31.p.rapidapi.com',
+  },
+};
+
+export const fetchFromAPI = async (url) => {
+  const { data } = await axios.get(`${BASE_URL}/${url}`, options);
+
+  return data;
+};
