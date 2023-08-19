2aa7f796ae7a876c589ccb031e7eea21c174ead2
unknown <kamatabhishek14@gmail.com>
2023-08-19 19:46:02 +0530

    ...

--- Pokedex_Project/vite-project/src/imgLink/Image.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-
-function Image(id) {
-
-    return `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`
-}
-
-export default Image
